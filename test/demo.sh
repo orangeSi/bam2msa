6565b5b73910d180b20008feb2c22ee4fd8470ba
root <root@awesome-ponies-2.localdomain>
2020-05-29 04:45:10 -0400

    update

--- test/demo.sh
+++ test/demo.sh
@@ -1,13 +1,13 @@
-set -vex
+set -e
 ./bam2msa ref.fa out.bwa.bam >out.bam2msa
-cat out.bam2msa |grep -v '^#'|awk '{print ">"$1"\n"$2"\n>"$3"\n"$4}' >out.bam2msa.msa
+cat out.bam2msa |grep -v '^#'|awk '{print ">"$1"\n"$3"\n>"$4"\n"$5}' >out.bam2msa.msa
 
 ln -sf  out.bam2msa.msa case
 ln -sf  ../data/out.mafft.msa control
 diff control case
 if [ $? -eq 0 ];
 then
-	echo test passed!
+	echo "ok, test passed!"
 else
-	echo test failed!
+	echo "sorry, test failed! give a issue to me please~~~"
 fi
